b23b142b8282ed228fcdaa32f189f6125d29de2f
Mstr0A <damargamer4@gmail.com>
2025-05-08 15:37:24 +0300

    Updated build file to use maven local User can now do .\gradlw pushToMavenLocal instead of having to do the dance of shadowjar Moved example to its own directory

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,68 +1,83 @@
 plugins {
     kotlin("jvm") version "2.1.10"
-    id("com.gradleup.shadow") version "8.3.6" // we use shadowJar to package the library here
+    id("com.gradleup.shadow") version "8.3.6"
+    `maven-publish`
+    signing
 }
 
-private val jdaVersion: String = "5.4.0" // to make changing the JDA version easier
-private val utilsVersion: String = "2.1" // to make changing the utils version easier
-private val loggingVersion: String = "7.0.4" // to make changing the logging version easier
-private val loggingAPIVersion: String = "2.0.17" // to make changing the logging API version easier
-private val logbackVersion: String = "1.5.16" // to make changing the logging API version easier
+////////////////////////////////////
+//                                //
+//     Project Configuration      //
+//                                //
+////////////////////////////////////
 
-group = "com.a0.kobalt"
+
+group = "com.a0"
 version = "0.0.1"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+kotlin {
+    jvmToolchain(8)
+}
 
 repositories {
     mavenCentral()
-
     maven { url = uri("https://m2.chew.pro/releases") }
 }
 
+val jdaVersion: String = "5.4.0"
+val utilsVersion: String = "2.1"
+val loggingVersion: String = "7.0.4"
+val loggingAPIVersion: String = "2.0.17"
+val logbackVersion: String = "1.5.16"
+
+
+////////////////////////////////////
+//                                //
+//    Dependency Configuration    //
+//                                //
+////////////////////////////////////
+
+
 dependencies {
-    // Main dependencies
-    implementation("net.dv8tion:JDA:$jdaVersion")
+
+    /* External dependencies */
+    api("net.dv8tion:JDA:$jdaVersion")
+    api("org.slf4j:slf4j-api:$loggingAPIVersion")
+
+    /* Internal dependencies */
     implementation("pw.chew:jda-chewtils:$utilsVersion")
-    implementation("org.slf4j:slf4j-api:$loggingAPIVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation(kotlin("reflect"))
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("io.github.oshai:kotlin-logging-jvm:$loggingVersion")
 
-    // Kotlin utilities
-    implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
-
-    // Test dependencies (keep even if unused)
+    /* Testing */
     testImplementation(kotlin("test"))
 }
 
-tasks {
-    // to specify the main entry point to the main file
-    jar {
-        manifest.attributes["Main-Class"] = "com.a0.kobalt.MainKt"
-    }
 
-    // to exclude the library file from getting compiled into the final version
+////////////////////////////////////
+//                                //
+//    GeneralTask Configuration   //
+//                                //
+////////////////////////////////////
+
+
+tasks {
     shadowJar {
         archiveBaseName.set("Kobalt")
         archiveVersion.set(project.version.toString())
         archiveClassifier.set("")
-
-        dependencies {
-            exclude(dependency("net.dv8tion:JDA:.*"))
-            exclude(dependency("pw.chew:jda-chewtils:.*"))
-            exclude(dependency("org.slf4j:slf4j-api:.*"))
-            exclude(dependency("ch.qos.logback:logback-classic:.*"))
-            exclude(dependency("io.github.oshai:kotlin-logging-jvm:.*"))
-        }
-        exclude(
-            "net/dv8tion/**",
-            "pw/chew/**",
-            "com/jagrosh/**",
-            "org/slf4j/**",
-            "ch/qos/logback/**",
-            "io/github/oshai/**",
-            "com/a0/kobalt/ExampleKt*",
-            "logback.xml"
-        )
+        manifest.attributes["Main-Class"] = "com.a0.kobalt.MainKt"
     }
 
     test {
@@ -70,6 +85,22 @@ tasks {
     }
 }
 
-kotlin {
-    jvmToolchain(21)
+
+////////////////////////////////////
+//                                //
+//    Publishing And Signing      //
+//                                //
+////////////////////////////////////
+
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "Kobalt"
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
 }
