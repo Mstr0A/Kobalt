d64fe56d040131fa0b263037e046ae418f53ed3a
Mstr0A <damargamer4@gmail.com>
2025-08-13 20:59:45 +0300

    Removed duplicate files in shared

--- src/main/kotlin/com/a0/kobalt/shared/exceptions/CommandException.kt
+++
@@ -1,7 +0,0 @@
-package com.a0.kobalt.shared.exceptions
-
-sealed class CommandException(
-    open val commandName: String,
-    message: String? = null,
-    cause: Throwable? = null
-) : Exception(message, cause)
